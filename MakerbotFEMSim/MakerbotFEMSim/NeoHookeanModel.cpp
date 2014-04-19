dc48ccac986ff6a15504bfe36be140105e8ea3a6
Lucas Smyk <lusmyk@mit.edu>
2014-04-19 16:16:22 -0400

    Cleaned up code spacing

--- MakerbotFEMSim/MakerbotFEMSim/NeoHookeanModel.cpp
+++ MakerbotFEMSim/MakerbotFEMSim/NeoHookeanModel.cpp
@@ -12,16 +12,14 @@ float NeoHookeanModel::getStrainEnergy(Eigen::Matrix3f defGrad)
 {
 	float IOne = (defGrad.transpose() * defGrad).trace();
 	float J    = log(defGrad.determinant());
-
-	float strainEnergy  = mu /2 * (IOne - 3) - mu*J + lambda / 2 * J * J;
+	float strainEnergy  = mu / 2 * (IOne - 3) - mu * J + lambda / 2 * J * J;
 	return strainEnergy;
 }
 
 Eigen::Matrix3f  NeoHookeanModel::firstPiolaStress(const Eigen::Matrix3f defGrad)
 {
 	float J    = log(defGrad.determinant());
-	Eigen::Matrix3f transposed = defGrad.transpose();
-	Eigen::Matrix3f invTranspose = transposed.inverse();
+	Eigen::Matrix3f invTranspose = defGrad.inverse().transpose();
 	return mu * (defGrad - invTranspose) + lambda * J * invTranspose;
 }
 
