64000e643bfd62ca81964965a66838653d02eee1
Lucas Smyk <lusmyk@mit.edu>
2014-04-20 13:16:21 -0400

    Added unnessary space

--- MakerbotFEMSim/MakerbotFEMSim/HexElement.h
+++ MakerbotFEMSim/MakerbotFEMSim/HexElement.h
@@ -14,6 +14,7 @@ public:
 	Eigen::Matrix3f defGradAtQuadPoint(Eigen::Vector3f p);
 	Eigen::Vector3f getForce(int vertexIndex);
 	Eigen::Vector3f getShapeFuncGrad(Eigen::Vector3f quadPoint, int ii);
+
 private:
 	Quadrature quadrature;
 	Eigen::Vector3f refPoints[NVERT];
