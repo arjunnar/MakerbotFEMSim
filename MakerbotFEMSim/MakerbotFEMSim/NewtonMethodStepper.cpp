47eb7148ebff77d417349b34fefb07043e1e469c
Lucas Smyk <lusmyk@mit.edu>
2014-05-14 22:27:46 -0400

    Merge

--- MakerbotFEMSim/MakerbotFEMSim/NewtonMethodStepper.cpp
+++ MakerbotFEMSim/MakerbotFEMSim/NewtonMethodStepper.cpp
@@ -159,8 +159,7 @@ std::vector<Triplet> NewtonMethodStepper::sparseStiffMat(ElementMesh * mesh)
 Eigen::VectorXf NewtonMethodStepper::getTotalForceVector(ElementMesh * mesh)
 {
 	Quadrature quadrature;
-	Eigen::Vector3f totalExternalForce(-0.02, -0.05,0);
-	
+    Eigen::Vector3f totalExternalForce(-0.02, -0.05,0);
 	int numNonFixedVertices = mesh->getNumNonFixedVertices();
 	Eigen::VectorXf totalForceVector(3*mesh->coords.size());
 	totalForceVector.setZero();
@@ -210,6 +209,7 @@ Eigen::VectorXf NewtonMethodStepper::getTotalForceVector(ElementMesh * mesh)
 
 	// REMOVE FIXED VERTICES FROM TOTAL FORCE VECTOR
 	Eigen::VectorXf newForce(3*numNonFixedVertices);
+    newForce.setZero();
 	int nonFixedCount = 0;
 	for (int ii = 0; ii < mesh->coords.size(); ++ii)
 	{
